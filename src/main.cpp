86b8cbb7b105ffe510ddfb55f3f2114dd75ed8b0
erikfla <erikfla@gmail.com>
2026-05-03 19:27:38 +0200

    feat: CF/ZF flagg i JSON state

--- src/main.cpp
+++ src/main.cpp
@@ -72,6 +72,10 @@ std::string toJSON() {
       << "\"CO\":"  << ((machine.activeControl() & CO)  ? "true" : "false") << ","
       << "\"J\":"   << ((machine.activeControl() & J)   ? "true" : "false") << ","
       << "\"FI\":"  << ((machine.activeControl() & FI)  ? "true" : "false")
+      << ","
+      << "\"flags\":{"
+        << "\"CF\":" << (machine.flagC() ? "true" : "false") << ","
+        << "\"ZF\":" << (machine.flagZ() ? "true" : "false")
       << "}}";
     return o.str();
 }
